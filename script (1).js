14285407b0623c8571a55dee024603d3b853cffc
alamgir701 <alamgirbadsha2001@gmail.com>
2024-12-27 20:04:55 +0530

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,29 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button) => {
+  button.addEventListener('click', (e) => {
+    if(e.target.innerHTML=='='){
+      try{
+        string=String(eval(string));
+        document.querySelector('input').value = string;
+      }
+      catch{
+        document.querySelector('input').value = 'ERROR';
+        string='';
+      }
+    }
+    else  if(e.target.innerHTML=='C'){
+      string="";
+      document.querySelector('input').value = string;
+    }
+    else  if(e.target.innerHTML=='←'){
+      string=string.slice(0,-1);
+      document.querySelector('input').value = string;
+    }
+    else{
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+    }
+  })
+})
